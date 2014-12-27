3bf80ef1d9abc1ec67c4c56d30d568bbdbe4cb18
fbourgey <bourgeyflorian@gmail.com>
2014-12-27 22:08:32 +0100

    On importe le fichier jar 'mini-project' On implémente MazeViewController dans la classe MazeViewControllerClass On retourne les méthodes par défaut de l'interface MazeViewController

--- src/maze/MazeViewControllerClass.java
+++ src/maze/MazeViewControllerClass.java
@@ -1,5 +1,38 @@
 package maze;
 
-public class MazeViewControllerClass {
+import fr.enst.inf103.ui.MazeViewController;
+import fr.enst.inf103.ui.MazeViewSource;
+
+public class MazeViewControllerClass implements MazeViewController {
+
+	@Override
+	public void calculateShortestPath() {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public MazeViewSource getMazeViewSource() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MazeViewSource newMaze() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public MazeViewSource openMaze(String arg0) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void saveMazeAs(String arg0) {
+		// TODO Auto-generated method stub
+		
+	}
 
 }
