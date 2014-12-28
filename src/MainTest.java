598d2ff13dc97ade92884ebc653242fe2f8a0808
fbourgey <bourgeyflorian@gmail.com>
2014-12-28 15:29:52 +0100

    suppression de lignes inutiles duês au tp précédent

--- src/MainTest.java
+++ src/MainTest.java
@@ -9,10 +9,7 @@ public class MainTest {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		
-		Maze maze= new Maze();
 		
-		maze.initFromTextFile("data/labyrinthe.txt");
-		maze.saveToTextFile("data/labyrinthe2.txt");
 		
 		MazeViewController mazeController= new MazeViewControllerClass();
 		MazeWindow mazeWindow = new MazeWindow("My awesome labyrinth", mazeController) ;
