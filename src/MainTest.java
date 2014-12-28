c3fa1e8fd63116d9caaf80ec243e30b6aa138f3a
Benjamin AUCLAIR <benjamin.auclair@telecom-paristech.fr>
2014-12-28 15:25:29 +0100

    Ajout de deux lignes de afin de permettre le run de l'application et d'afficher la fenêtre avec le labyrinthe

--- src/MainTest.java
+++ src/MainTest.java
@@ -1,5 +1,7 @@
+import fr.enst.inf103.ui.MazeViewController;
 import fr.enst.inf103.ui.MazeWindow;
 import maze.Maze;
+import maze.MazeViewControllerClass;
 
 
 public class MainTest {
@@ -11,6 +13,9 @@ public class MainTest {
 		
 		maze.initFromTextFile("data/labyrinthe.txt");
 		maze.saveToTextFile("data/labyrinthe2.txt");
+		
+		MazeViewController mazeController= new MazeViewControllerClass();
+		MazeWindow mazeWindow = new MazeWindow("My awesome labyrinth", mazeController) ;
 
 	}
 
